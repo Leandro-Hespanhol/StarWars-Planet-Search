6b46a26be8ca9cab1ba2a3ab10c4c6a3789dea74
Leandro Hespanhol <leandrohespanhol@gmail.com>
2021-12-23 13:03:44 -0300

    Updates

---
+++ precache-manifest.65093d313e9f5e1177cffcb01870b6fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "375250731d38a1ce82bb5cb748e2c2df",
+    "url": "/star-wars-planet-search/index.html"
+  },
+  {
+    "revision": "cbd97b484f242ff49135",
+    "url": "/star-wars-planet-search/static/css/main.60921d2e.chunk.css"
+  },
+  {
+    "revision": "5314ba40c597b27a4872",
+    "url": "/star-wars-planet-search/static/js/2.e6095c48.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/star-wars-planet-search/static/js/2.e6095c48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbd97b484f242ff49135",
+    "url": "/star-wars-planet-search/static/js/main.750c0c6d.chunk.js"
+  },
+  {
+    "revision": "430eadddf8313e3d766d",
+    "url": "/star-wars-planet-search/static/js/runtime-main.2983b150.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/starWars-planet-search/precache-manifest.1c699327eb8511eebd52aae77cf2bf82.js"
+  "/star-wars-planet-search/precache-manifest.65093d313e9f5e1177cffcb01870b6fe.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/starWars-planet-search/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/star-wars-planet-search/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
